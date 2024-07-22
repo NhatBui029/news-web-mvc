236ca2a1f8ec367f1a9866b26a0be58a9ff318b3
NhatBui029 <buituannhat1402@gmail.com>
2024-07-22 14:31:07 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+const morgan = require('morgan')
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT || 3000;
+
+app.use(morgan('dev'))
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server");
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
