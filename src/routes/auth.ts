3c662faeb88b6cabbf4d08c216fbb154a4d0224a
NhatBui029 <buituannhat1402@gmail.com>
2024-07-22 16:14:01 +0700

    add hbs

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,7 +3,9 @@ import express, { Request, Response } from "express";
 const route = express.Router();
 
 route.get('/',(req: Request, res: Response) => {
-    res.send('hello')
+    res.render('auth/login', {
+        layout: 'auth'
+    })
 })
 
 module.exports= route;
